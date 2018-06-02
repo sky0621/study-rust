bec56ba6aa15192b67407c171072b6a3354cda77
sky0621 <sky14work@gmail.com>
2018-06-03 00:11:16 +0900

    continue

--- chap03_Learn_Rust/chap0301_guessing_game/guessing_game/src/main.rs
+++ chap03_Learn_Rust/chap0301_guessing_game/guessing_game/src/main.rs
@@ -1,8 +1,16 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     // 「!」付きはマクロ
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
     println!("Please input your guess.");
 
     // 「let文」＝変数束縛を作る
@@ -13,7 +21,8 @@ fn main() {
     // 「::new()」＝関連関数
     let mut guess = String::new();
 
-    // 
+    // 「stdin()」＝ターミナルへの標準入力
+    // 「read_line()」＝「io::Result」を返す
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
