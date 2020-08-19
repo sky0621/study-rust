103f4b2cb5dce1e440f09e2c38a52fc9eddec71f
sky0621 <sky14work@gmail.com>
2020-08-19 22:51:52 +0900

    temp

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,19 +1,46 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
-    println!("Please input your guess.");
 
-    // let ... 変数宣言。immutable
-    // mut ... mutable
-    // String::new() ... static method
-    let mut guess = String::new();
+    // default ... i32
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    println!("The secret number is {}", secret_number);
 
-    // read_line() ... return 'Result<usize>'
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    loop {
+        println!("Please input your guess.");
 
-    // {} ... プレースホルダー
-    println!("You guessed: {}", guess);
+        // let ... 変数宣言。immutable
+        // mut ... mutable
+        // String::new() ... static method
+        let mut guess = String::new();
+
+        // read_line() ... return 'Result<usize>'
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        // shadowing
+        // unsigned 32 bit
+        // parse() ... return Result
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        // {} ... プレースホルダー
+        println!("You guessed: {}", guess);
+
+        // match 式
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+            Ordering::Greater => println!("Too big!"),
+        }
+    }
 }
