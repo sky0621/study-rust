daa4c6a1df69d27935ffd642f2c0941d55167fa8
sky0621 <sky14work@gmail.com>
2020-08-28 21:24:24 +0900

    temp

--- book_se_ch9/src/main.rs
+++ book_se_ch9/src/main.rs
@@ -1,3 +1,31 @@
+use std::fs::File;
+use std::io::ErrorKind;
+
 fn main() {
-    println!("Hello, world!");
+    let f = File::open("hello.txt");
+    let f = match f {
+        Ok(file) => file,
+        Err(ref error) if error.kind() == ErrorKind::NotFound => match File::create("hello.txt") {
+            Ok(fc) => fc,
+            Err(e) => {
+                panic!(e);
+            }
+        },
+        Err(err) => {
+            panic!(err);
+        }
+    };
+    sub();
+
+    let f2 = File::open("hello3.txt").unwrap();
+}
+
+fn sub() {
+    let f = File::create("hello2.txt");
+    let f = match f {
+        Ok(fl) => fl,
+        Err(e) => {
+            panic!("Tried to create file but there was a problem: {:?}", e);
+        }
+    };
 }
