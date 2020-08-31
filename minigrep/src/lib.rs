e9b17644c57221ae8915f0175eeccadef284daab
sky0621 <sky14work@gmail.com>
2020-08-31 22:59:37 +0900

    temp

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::error::Error;
 use std::fs::File;
 use std::io::Read;
@@ -10,7 +11,12 @@ pub fn run(cfg: Config) -> Result<(), Box<Error>> {
     // ?演算子は、現在の関数からエラー値を返す
     f.read_to_string(&mut contents)?;
 
-    for line in search(&cfg.query, &contents) {
+    let results = if cfg.case_sensitive {
+        search(&cfg.query, &contents)
+    } else {
+        search_case_insensitive(&cfg.query, &contents)
+    };
+    for line in results {
         println!("{}", line);
     }
 
@@ -20,6 +26,7 @@ pub fn run(cfg: Config) -> Result<(), Box<Error>> {
 pub struct Config {
     query: String,
     filename: String,
+    case_sensitive: bool,
 }
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
@@ -28,7 +35,12 @@ impl Config {
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config {
+            query,
+            filename,
+            case_sensitive,
+        })
     }
 }
 
@@ -42,12 +54,23 @@ pub fn search<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &'a str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         // Rustは
         // 安全で速く生産性も高い。
@@ -55,8 +78,24 @@ mod test {
         let contents = "\
 Rust:
 safe, fast, productive.
-Pick three.";
+Pick three.
+Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,12 +8,12 @@ use std::{env, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
     let cfg = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = minigrep::run(cfg) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
