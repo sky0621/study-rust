168509bcd0196e97e3dc972d3e3ba51bb35a5f62
sky0621 <sky14work@gmail.com>
2020-08-31 21:36:57 +0900

    temp

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,1 +1,31 @@
-fn run() {}
+use std::error::Error;
+use std::fs::File;
+use std::io::Read;
+
+// Box<Error> ... トレイトオブジェクト(関数がErrorトレイトを実装する型を返すことを意味する)
+pub fn run(cfg: Config) -> Result<(), Box<Error>> {
+    let mut f = File::open(cfg.filename).expect("file not found");
+
+    let mut contents = String::new();
+    // ?演算子は、現在の関数からエラー値を返す
+    f.read_to_string(&mut contents)?;
+
+    println!("With text:\n{}", contents);
+
+    Ok(())
+}
+
+pub struct Config {
+    query: String,
+    filename: String,
+}
+impl Config {
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,57 +1,50 @@
-use std::env;
+extern crate minigrep;
+
+use minigrep::Config;
 use std::fs::File;
 use std::io::Read;
+use std::{env, process};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let cfg = Config::new(&args);
+    let cfg = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    let mut f = File::open(cfg.filename).expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    println!("With text:\n{}", contents);
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Config { query, filename }
+    if let Err(e) = minigrep::run(cfg) {
+        println!("Application error: {}", e);
+        process::exit(1);
     }
 }
 
-fn prg01() {
-    let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
-}
-
-fn prg02() {
-    let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    // {}を探しています
-    println!("Searching for {}", query);
-    // {}というファイルの中
-    println!("In file {}", filename);
-}
-
-fn prg03() {
-    let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
-    // {}を探しています
-    println!("Searching for {}", query);
-    // {}というファイルの中
-    println!("In file {}", filename);
-
-    let mut f = File::open(filename).expect("file not found");
-    let mut contents = String::new();
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file");
-    println!("With text:\n{}", contents);
-}
+// fn prg01() {
+//     let args: Vec<String> = env::args().collect();
+//     println!("{:?}", args);
+// }
+//
+// fn prg02() {
+//     let args: Vec<String> = env::args().collect();
+//     let query = &args[1];
+//     let filename = &args[2];
+//     // {}を探しています
+//     println!("Searching for {}", query);
+//     // {}というファイルの中
+//     println!("In file {}", filename);
+// }
+//
+// fn prg03() {
+//     let args: Vec<String> = env::args().collect();
+//     let query = &args[1];
+//     let filename = &args[2];
+//     // {}を探しています
+//     println!("Searching for {}", query);
+//     // {}というファイルの中
+//     println!("In file {}", filename);
+//
+//     let mut f = File::open(filename).expect("file not found");
+//     let mut contents = String::new();
+//     f.read_to_string(&mut contents)
+//         .expect("something went wrong reading the file");
+//     println!("With text:\n{}", contents);
+// }
