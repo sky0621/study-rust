a197511203916463eef146cd9a6397294eb5fc82
sky0621 <sky14work@gmail.com>
2020-09-01 23:08:11 +0900

    temp

--- book_se_ch13/src/main.rs
+++ book_se_ch13/src/main.rs
@@ -1,9 +1,17 @@
 mod lib;
 
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
+    // let a = |num| num * num;
+    // println!("{}", a(16));
+    //
+    // let x = 4;
+    // let equal_to_x = |z| z == x;
+    // let y = 4;
+    // println!("{}", equal_to_x(y));
 
-    let a = |num| num * num;
-    println!("{}", a(16));
+    let xx = vec![1, 2, 3];
+    let eq_to_xx = move |z| z == xx;
+    println!("{:?}", xx);
+    let yy = vec![1, 2, 3];
+    println!("{:?}", eq_to_xx(yy));
 }
