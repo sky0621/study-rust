175e7c6f77f52c998a07e90836043e58ae1eb051
sky0621 <sky14work@gmail.com>
2020-09-11 23:04:15 +0900

    temp

--- rust-by-example/chXX/src/main.rs
+++ rust-by-example/chXX/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let s = dangle();
+    println!("{}", s);
+    let s2 = &s;
+    println!("{}", s2);
+    println!("{}", s);
+    println!("{}", *s2);
+}
+
+fn dangle() -> String {
+    let s = String::from("abc");
+    s
 }
