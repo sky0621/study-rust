2784b300bf398440ee87a9b7e24120b3e44b6e13
sky0621 <sky14work@gmail.com>
2020-09-27 22:05:30 +0900

    temp

--- book_se/ch20_1/src/main.rs
+++ book_se/ch20_1/src/main.rs
@@ -1,6 +1,8 @@
 use std::fs::File;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
+use std::thread;
+use std::time::Duration;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -16,6 +18,7 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buf = [0; 1024];
     stream.read(&mut buf).unwrap();
 
+    thread::sleep(Duration::from_secs(5));
     let mut f = File::open("hello.html").unwrap();
     let mut contents = String::new();
     f.read_to_string(&mut contents).unwrap();
